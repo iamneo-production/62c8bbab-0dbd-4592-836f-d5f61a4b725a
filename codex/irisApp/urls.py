fd4dd9808d4f30688cbed1cbecb6df64f26ed2e3
anand-harsh <harsh.anand.ggl@gmail.com>
2023-02-14 17:34:14 +0000

    Update DemoModel #2

--- codex/irisApp/urls.py
+++ codex/irisApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from rest_framework import routers
 
 urlpatterns=[
-    path('', views.predictor)
+    path('', views.predictor),
+    path('result/', views.formInfo, name=result)
 ]
--- codex/irisApp/views.py
+++ codex/irisApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from joblib import load
+model=load('./savedModels/model.joblib')
 
 # Create your views here.
 def predictor(request):
@@ -9,4 +11,5 @@ def formInfo(request):
     sepal_width=request.GET['sepal_width']
     petal_length=request.GET['petal_length']
     petal_width=request.GET['petal_width']
+    y_pred=model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
     return render(request, 'result.html')
