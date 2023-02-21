fcd099d683748dacceadfd7930b1d6e832df15fc
anand-harsh <harsh.anand.ggl@gmail.com>
2023-02-21 10:48:48 +0000

    Update Pipeline#7

--- codex/aqiApp/urls.py
+++ codex/aqiApp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
  
-    path('', views.aqipredictor, name='predict'),
+    path('', views.aqipredictor, name='predict-aqi'),
     path('result/', views.predict_aqi, name='result')
     
 ]
--- codex/codex/settings.py
+++ codex/codex/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'codexui/build/static')
+    os.path.join(BASE_DIR, 'codexui/public')
 ]
